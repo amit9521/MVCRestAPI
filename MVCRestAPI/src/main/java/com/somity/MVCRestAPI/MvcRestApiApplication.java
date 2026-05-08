644f35c28661d4ab9a093a1116c3b44dbe764fea
amit9521 <amit160199@gmail.com>
2026-05-08 17:57:44 +0530

    Initial commit

---
+++ MVCRestAPI/src/main/java/com/somity/MVCRestAPI/MvcRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.somity.MVCRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRestApiApplication.class, args);
+	}
+
+}
---
+++ MVCRestAPI/src/test/java/com/somity/MVCRestAPI/MvcRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somity.MVCRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
